546fb38ae99f1811e860854470f06180894bd45b
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2024-04-27 23:42:03 +0700

    test db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\DatabaseTest;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-database-connection', function () {
+    return DatabaseTest::testConnection();
 });

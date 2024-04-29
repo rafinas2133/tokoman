19a2f4b1c41f54644c2848da0ed3bee37348de27
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-04-29 11:18:26 +0700

    update name apps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Tokoman') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <title>Laravel</title>
+        <title>Tokoman</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

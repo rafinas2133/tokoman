00fef0431c1c5fc8985ff1596fa790a6247a37f8
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-05-04 18:17:08 +0700

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" {{ $attributes }}>
-    <image xlink:href="https://tokoman.s3.ap-southeast-2.amazonaws.com/asset/logo.png" width="100" height="100" />
+<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" {{ $attributes }}>
+    <image xlink:href="https://tokoman.s3.ap-southeast-2.amazonaws.com/asset/logo.png" width="90" height="90" />
 </svg>

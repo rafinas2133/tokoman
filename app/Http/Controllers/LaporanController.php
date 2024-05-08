3b33b0d4637c6dc3f468a8071a259a6d099fdf9a
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-05-08 15:07:56 +0700

    laporan mingguan :pig:

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\StokBarang;
 use App\Models\Laporan;
@@ -22,13 +23,16 @@ class LaporanController extends Controller
 
         for ($i = 1; $i <= $request->line; $i++) {
             $validator = Validator::make($request->all(), [
-                'reportDate'.$i => 'required|date',
+                'reportDate'.$i => 'required|date|before_or_equal:' . Carbon::now()->toDateString().
+                '|after_or_equal:' . Carbon::now()->subWeek()->toDateString(),
                 'itemName'.$i => 'required|integer',
                 'itemQuantity'.$i => 'required|integer',
             ],
             [
                 'required' => 'Data tidak boleh kosong',
                 'integer' => 'Data harus angka',
+                'before_or_equal' => 'Tanggal harus seminggu - tanggal sekarang',
+                'after_or_equal' => 'Tanggal harus seminggu - tanggal sekarang',
             ]);
             
             if ($validator->fails()) {

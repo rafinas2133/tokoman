4122065c0df55417497dc3dd5b5bc04d4f3cb9ac
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-05-08 14:35:01 +0700

    memodelkan modal and trigger

--- database/migrations/2024_05_05_113437_create_riwayat_table.php
+++ database/migrations/2024_05_05_113437_create_riwayat_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,6 +20,29 @@ return new class extends Migration
             $table->foreign('id_barang')->references('id')->on('stok_barangs')->onDelete('cascade');
             $table->timestamps();
         });
+        $triggerSQL = "
+        CREATE TRIGGER trigger_modal
+        AFTER INSERT ON riwayat
+        FOR EACH ROW
+        BEGIN
+        DECLARE total DECIMAL(10, 2);
+    
+        -- Calculate total modal
+        SELECT (r.jumlah * sb.harga_beli) 
+        INTO total
+        FROM riwayat r
+        LEFT JOIN stok_barangs sb ON r.id_barang = sb.id
+        WHERE r.jenis_riwayat = 'masuk'
+        ORDER BY r.id DESC -- Urutkan berdasarkan tanggal secara descending
+        LIMIT 1; -- Batasi hasil ke satu record terbaru
+    
+        -- Insert total into Modal table
+        INSERT INTO Modal (Total_modal, Tanggal, created_at, updated_at)
+        VALUES (total, NOW(), NOW(), NOW());
+        END;
+        ";
+        DB::unprepared($triggerSQL);
+
     }
 
     /**
@@ -28,6 +50,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        $triggerSQL = "DROP TRIGGER IF EXISTS trigger_modal;";
+        DB::unprepared($triggerSQL);
         Schema::dropIfExists('riwayat');
     }
 };

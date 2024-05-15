86ea67d8789f7c951df1a1c889068f6277be1a55
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2024-05-15 16:36:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,32 @@ use App\Http\Controllers\dashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\barangController;
 use App\Http\Controllers\SearchStok;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\pegawaiController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\ProfitController;
 use App\Http\Controllers\riwayatController;
+use Illuminate\Support\Facades\Route;
 
 // Route untuk user terAuth()
 Route::middleware(['auth', 'verified', 'noback'])->group(function () {
     Route::post('/export-profit', [dashboardController::class, 'exportPDF'])->name('exportProfit');
     Route::post('/export-pdf', [riwayatController::class, 'exportPDF'])->name('exportPDF');
-    //Route Semua Role User
+
+    // Route Semua Role User
     Route::prefix('riwayat')->group(function () {
         Route::get('/', [riwayatController::class, 'tampilkan'])->name('riwayat');
         Route::get('/filter', [riwayatController::class, 'filterResults'])->name('riwayatFilter');
     });
+
     Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
-    //laporan dan profit
+
+    // Laporan dan profit
     Route::get('/pelaporan', [LaporanController::class, 'index'])->name('laporan');
     Route::get('/hitungProfit', [ProfitController::class, 'index'])->name('profit.index');
     Route::post('/pelaporan', [LaporanController::class, 'laporanPost'])->name('pelaporan.postData');
-    //Manipulasi Stok
+
+    // Manipulasi Stok
     Route::prefix('stok')->group(function () {
         Route::get('/', [barangController::class, 'adminIndex'])->name('stokIndex');
         Route::get('/search', [SearchStok::class, 'index'])->name('searchStokadmin');
@@ -35,6 +39,7 @@ Route::middleware(['auth', 'verified', 'noback'])->group(function () {
         Route::post('/editsave/{id}', [barangController::class, 'editsave'])->name('editSave');
         Route::post('/delete/{id}', [barangController::class, 'delete'])->name('deleteStok');
         Route::post('/deleteAll', [barangController::class, 'deleteAll'])->name('deleteStokAll');
+
         Route::get('/addsave', function () {
             return redirect()->route('stokBarang');
         });
@@ -47,19 +52,20 @@ Route::middleware(['auth', 'verified', 'noback'])->group(function () {
         Route::get('/deleteAll', function () {
             return redirect()->route('stokBarang');
         });
-
     });
+
     Route::post('/tambahstok/{id}', [barangController::class, 'tambahStok'])->name('tambahStok');
     Route::post('/deleteImg/{id}', [barangController::class, 'deleteImg'])->name('deletegambar');
 
-    //Mentalin Request Post saat direquest Get
+    // Mentalin Request Post saat direquest Get
     Route::get('/tambahstok/{id}', function () {
         return redirect()->route('stokBarang');
     });
     Route::get('/deleteImg/{id}', function () {
         return redirect()->route('stokBarang');
     });
-    //Khusus Admin
+
+    // Khusus Admin
     Route::prefix('admin')->middleware(['admin'])->group(function () {
         Route::get('/', [pegawaiController::class, 'index'])->name('Manajemen.Admin');
         Route::get('/add', [pegawaiController::class, 'add'])->name('Tambah.Pegawai');
@@ -68,7 +74,7 @@ Route::middleware(['auth', 'verified', 'noback'])->group(function () {
         Route::post('/editsave/{id}', [pegawaiController::class, 'editsave'])->name('Editkan.Pegawai');
         Route::post('/delete/{id}', [pegawaiController::class, 'delete'])->name('Hapus.Pegawai');
         Route::post('/deleteAll', [pegawaiController::class, 'deleteAll'])->name('Hapus.AllPegawai');
-        //Mentalin Request Post saat direquest Get
+
         Route::get('/editsave/{id}', function () {
             return redirect()->route('Manajemen.Admin');
         });
@@ -83,10 +89,12 @@ Route::middleware(['auth', 'verified', 'noback'])->group(function () {
         });
     });
 });
-//Route Guest unAuth()
+
+// Route Guest unAuth()
 Route::get('/', [barangController::class, 'index'])->middleware('noback')->name('stokBarang');
 Route::get('/wa/{id}', [barangController::class, 'reqWa']);
-//RouteSearch
+
+// RouteSearch
 Route::get('/search', [SearchController::class, 'index'])->name('search');
 
 Route::middleware(['auth', 'noback'])->group(function () {

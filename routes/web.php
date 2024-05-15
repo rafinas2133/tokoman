f9e872da7dcea82c1aadb0f59955526da58c637d
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-05-15 15:26:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,47 @@ use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\ProfitController;
 use App\Http\Controllers\riwayatController;
 
-// Route::post('/testingAPI123', [barangController::class,'addsave'])->name('stokSavetest');
 // Route untuk user terAuth()
 Route::middleware(['auth', 'verified', 'noback'])->group(function () {
-    Route::post('/export-profit', [dashboardController::class,'exportPDF'])->name('exportProfit');
+    Route::post('/export-profit', [dashboardController::class, 'exportPDF'])->name('exportProfit');
     Route::post('/export-pdf', [riwayatController::class, 'exportPDF'])->name('exportPDF');
     //Route Semua Role User
-    Route::get('/riwayat', [riwayatController::class, 'tampilkan'])->name('riwayat');
-    Route::get('/riwayat/filter', [riwayatController::class, 'filterResults'])->name('riwayatFilter');
+    Route::prefix('riwayat')->group(function () {
+        Route::get('/', [riwayatController::class, 'tampilkan'])->name('riwayat');
+        Route::get('/filter', [riwayatController::class, 'filterResults'])->name('riwayatFilter');
+    });
     Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
     //laporan dan profit
     Route::get('/pelaporan', [LaporanController::class, 'index'])->name('laporan');
     Route::get('/hitungProfit', [ProfitController::class, 'index'])->name('profit.index');
     Route::post('/pelaporan', [LaporanController::class, 'laporanPost'])->name('pelaporan.postData');
     //Manipulasi Stok
-    Route::get('/stok', [barangController::class, 'adminIndex'])->name('stokIndex');
-    Route::get('/stok/search', [SearchStok::class, 'index'])->name('searchStokadmin');
+    Route::prefix('stok')->group(function () {
+        Route::get('/', [barangController::class, 'adminIndex'])->name('stokIndex');
+        Route::get('/search', [SearchStok::class, 'index'])->name('searchStokadmin');
+        Route::get('/add', [barangController::class, 'add'])->name('tambahBarang');
+        Route::post('/addsave', [barangController::class, 'addsave'])->name('stokSave');
+        Route::get('/edit/{id}', [barangController::class, 'edit'])->name('editStok');
+        Route::post('/editsave/{id}', [barangController::class, 'editsave'])->name('editSave');
+        Route::post('/delete/{id}', [barangController::class, 'delete'])->name('deleteStok');
+        Route::post('/deleteAll', [barangController::class, 'deleteAll'])->name('deleteStokAll');
+        Route::get('/addsave', function () {
+            return redirect()->route('stokBarang');
+        });
+        Route::get('/editsave/{id}', function () {
+            return redirect()->route('stokBarang');
+        });
+        Route::get('/delete/{id}', function () {
+            return redirect()->route('stokBarang');
+        });
+        Route::get('/deleteAll', function () {
+            return redirect()->route('stokBarang');
+        });
+
+    });
     Route::post('/tambahstok/{id}', [barangController::class, 'tambahStok'])->name('tambahStok');
     Route::post('/deleteImg/{id}', [barangController::class, 'deleteImg'])->name('deletegambar');
-    Route::get('/stok/add', [barangController::class, 'add'])->name('tambahBarang');
-    Route::post('/stok/addsave', [barangController::class, 'addsave'])->name('stokSave');
-    Route::get('/stok/edit/{id}', [barangController::class, 'edit'])->name('editStok');
-    Route::post('/stok/editsave/{id}', [barangController::class, 'editsave'])->name('editSave');
-    Route::post('/stok/delete/{id}', [barangController::class, 'delete'])->name('deleteStok');
-    Route::post('/stok/deleteAll', [barangController::class, 'deleteAll'])->name('deleteStokAll');
+
     //Mentalin Request Post saat direquest Get
     Route::get('/tambahstok/{id}', function () {
         return redirect()->route('stokBarang');
@@ -42,44 +59,29 @@ Route::middleware(['auth', 'verified', 'noback'])->group(function () {
     Route::get('/deleteImg/{id}', function () {
         return redirect()->route('stokBarang');
     });
-    Route::get('/stok/addsave', function () {
-        return redirect()->route('stokBarang');
-    });
-    Route::get('/stok/editsave/{id}', function () {
-        return redirect()->route('stokBarang');
-    });
-    Route::get('/stok/delete/{id}', function () {
-        return redirect()->route('stokBarang');
-    });
-    Route::get('/stok/deleteAll', function () {
-        return redirect()->route('stokBarang');
-    });
     //Khusus Admin
-    Route::middleware(['admin'])->group(function () {
-
-        //Manajemen Pegawai
-        Route::get('/admin', [pegawaiController::class, 'index'])->name('Manajemen.Admin');
-        Route::get('admin/add', [pegawaiController::class, 'add'])->name('Tambah.Pegawai');
-        Route::post('admin/addsave', [pegawaiController::class, 'addSave'])->name('Tambahkan.Pegawai');
-        Route::get('admin/edit/{id}', [pegawaiController::class, 'edit'])->name('Edit.Pegawai');
-        Route::post('admin/editsave/{id}', [pegawaiController::class, 'editsave'])->name('Editkan.Pegawai');
-        Route::post('admin/delete/{id}', [pegawaiController::class, 'delete'])->name('Hapus.Pegawai');
-        Route::post('admin/deleteAll', [pegawaiController::class, 'deleteAll'])->name('Hapus.AllPegawai');
+    Route::prefix('admin')->middleware(['admin'])->group(function () {
+        Route::get('/', [pegawaiController::class, 'index'])->name('Manajemen.Admin');
+        Route::get('/add', [pegawaiController::class, 'add'])->name('Tambah.Pegawai');
+        Route::post('/addsave', [pegawaiController::class, 'addSave'])->name('Tambahkan.Pegawai');
+        Route::get('/edit/{id}', [pegawaiController::class, 'edit'])->name('Edit.Pegawai');
+        Route::post('/editsave/{id}', [pegawaiController::class, 'editsave'])->name('Editkan.Pegawai');
+        Route::post('/delete/{id}', [pegawaiController::class, 'delete'])->name('Hapus.Pegawai');
+        Route::post('/deleteAll', [pegawaiController::class, 'deleteAll'])->name('Hapus.AllPegawai');
         //Mentalin Request Post saat direquest Get
-        Route::get('admin/editsave/{id}', function () {
+        Route::get('/editsave/{id}', function () {
             return redirect()->route('Manajemen.Admin');
         });
-        Route::get('admin/delete/{id}', function () {
+        Route::get('/delete/{id}', function () {
             return redirect()->route('Manajemen.Admin');
         });
-        Route::get('admin/deleteAll', function () {
+        Route::get('/deleteAll', function () {
             return redirect()->route('Manajemen.Admin');
         });
-        Route::get('admin/addsave', function () {
+        Route::get('/addsave', function () {
             return redirect()->route('Manajemen.Admin');
         });
     });
-
 });
 //Route Guest unAuth()
 Route::get('/', [barangController::class, 'index'])->middleware('noback')->name('stokBarang');

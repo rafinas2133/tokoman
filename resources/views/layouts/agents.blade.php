3c46d52ec5d5dde0ea44a9424480bf35cb06fea7
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2024-05-24 00:45:38 +0700

    minor fix agent

--- resources/views/layouts/agents.blade.php
+++ resources/views/layouts/agents.blade.php
@@ -1,24 +1,37 @@
 <div class="container mx-auto px-4 dark:text-white text-black">
     <h1 class="text-3xl text-center font-bold my-4">Agents</h1>
-    <a href="{{ route('agents.add') }}" class="bg-blue-500 px-4 py-2 rounded hover:bg-gray-700 hover:rounded-md hover:shadow">Add agents</a>
+    <a href="{{ route('agents.add') }}"
+        class="bg-blue-500 px-4 py-2 rounded hover:bg-gray-700 hover:rounded-md hover:shadow">Add agents</a>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-4">
         @foreach ($agents as $agent)
-            <div class="dark:bg-gray-500 bg-white p-4 rounded shadow">
-                <h2 class="text-xl font-bold">{{ $agent->name }}</h2>
-                <!-- ngene lho cok -->
-                <!-- <p class=" break-words">{{ $agent->address }} ea gung rapi aokwokwowkok</p> -->
-                <p>{{ $agent->address}}</p>
-                <p>{{ $agent->noTelp }}</p>
-                <div>
-                    <img src="https://tokoman.s3.ap-southeast-2.amazonaws.com/agents/{{$agent->images}}" alt="Image" class="w-full h-32 object-cover my-2">
+            <div class="dark:bg-gray-500 bg-white p-4 rounded shadow relative">
+                <div class="flex justify-between items-center pb-4" x-data="{ open: false }">
+                    <h1 class="text-xl font-bold">{{ $agent->name }}</h1>
+                    <button @click="open = !open"
+                        class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button">
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                        </svg>
+                    </button>
+                    <div x-show="open" @click.away="open = false"
+                        class="absolute right-0 mr-3 mt-2 top-12 w-24 bg-white dark:bg-gray-700 rounded-md shadow-lg p-1 z-20">
+                        <a href="{{ route('agents.edit', $agent->id) }}"
+                            class="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">Edit</a>
+                        <form action="{{ route('agents.destroy', $agent->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit"
+                                class="w-full text-left block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">Delete</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="flex md:max-[840px]:flex-col gap-4 justify-center xitems-center">
-                    <a href="{{ route('agents.edit', $agent->id) }}" class="bg-green-500 px-4 py-2 rounded w-fit hover:bg-gray-700 hover:rounded-md hover:shadow">Edit</a>
-                    <form action="{{ route('agents.destroy', $agent->id) }}" method="POST" class="">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="bg-red-500 px-4 py-2 rounded hover:bg-gray-700 hover:rounded-md hover:shadow">Delete</button>
-                    </form>
+                <p class=" break-words">{{ $agent->address}}</p>
+                <p>{{ $agent->noTelp }}</p>
+                <div class>
+                    <img src="https://tokoman.s3.ap-southeast-2.amazonaws.com/agents/{{$agent->images}}" alt="Image"
+                        class="w-full h-60 object-cover my-2">
                 </div>
             </div>
         @endforeach

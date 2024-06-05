622ef003f5466ece06bcefd2b472dd5c72796282
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-06-05 17:42:47 +0700

    Update LandingContent.blade.php

--- resources/views/layouts/LandingContent.blade.php
+++ resources/views/layouts/LandingContent.blade.php
@@ -28,7 +28,7 @@
                 <button
                     class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded w-full mb-3"
                     type="submit">
-                    Search
+                    Cari
                 </button>
 
                 <!-- Dashboard Link -->

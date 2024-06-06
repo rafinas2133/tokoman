3ace815249a89ef25e547471451820c50382dc63
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-06-06 13:28:25 +0700

    Update pelaporanAdd.blade.php

--- resources/views/layouts/pelaporanAdd.blade.php
+++ resources/views/layouts/pelaporanAdd.blade.php
@@ -51,7 +51,7 @@ $line = 1;
                         <div class="mt-4">
                             <button type="button" id="addLineBtn"
                                 class="px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Tambah
-                                laporan lain</button>
+                                penjualan lain</button>
                         </div>
                         <div class="mt-4">
                             <button type="button" onclick="validasiForm()"

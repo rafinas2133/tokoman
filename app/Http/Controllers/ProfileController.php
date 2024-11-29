e61074c9df9acaf9075e66aa8e3b73f342bf73e6
fafa123id <104996246+fafa123id@users.noreply.github.com>
2024-11-29 11:01:19 +0700

    Revert "Update ProfileController.php"
    This reverts commit 4cda0361a9f9f63171e502fa8ee4120c83b94af0.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,7 +22,6 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
-        Auth::attempt(['email' => 'admin1@admin.admin', 'password' => '12345678']);
         return view('profile.edit', [
             'user' => $request->user(),
         ]);

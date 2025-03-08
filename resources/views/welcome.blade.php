ea2238269854c459077d468af8b293cbc792bdc4
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-03-08 11:48:58 +0700

    production fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DeslJHZx.css') }}">
+    <script src="{{ asset('build/assets/app-BERQ7i9F.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">

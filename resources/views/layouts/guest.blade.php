1cd241f700b55ae4cc69c67a2884f6256cd77fe7
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-21 02:48:20 +0700

    fix hardcoding vite build assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ asset('build/assets/app-DeslJHZx.css') }}">
-        <script src="{{ asset('build/assets/app-BERQ7i9F.js') }}" defer></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div id="mainApp" class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ asset('build/assets/app-DeslJHZx.css') }}">
-    <script src="{{ asset('build/assets/app-BERQ7i9F.js') }}" defer></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">

835ebcf87e1a8ae13e6524d3b906b36e5802bd80
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-21 03:25:14 +0700

    improve entrypoint to speed up build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,6 @@ set -e
 php artisan config:clear
 php artisan package:discover
 
-echo "Waiting for database to be ready..."
-until nc -z -v -w30 db-mysql 3306
-do
-  echo "Database is not ready yet. Retrying in 2 seconds..."
-  sleep 2
-done
-echo "Database is ready!"
-
 php artisan key:generate --force
 php artisan optimize:clear
 php artisan optimize

5b74269ef8f1d9c3195772c29de117383c070eb9
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-29 06:04:36 +0700

    update config timezone to default asia/jakarta

--- config/app.php
+++ config/app.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
 
     /*
     |--------------------------------------------------------------------------

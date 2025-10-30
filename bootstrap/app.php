26040332454bc3da27c05226214e9ad744b12572
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-30 20:20:29 +0700

    revert to trust all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,33 +31,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['edited' => \App\Http\Middleware\editedLogout::class]);
     })->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['greateradmin' => \App\Http\Middleware\rootadmin::class]);
-    })->withMiddleware(function (Middleware $middleware) {
-        $cfCidrs = [
-            '103.21.244.0/22',
-            '103.22.200.0/22',
-            '103.31.4.0/22',
-            '104.16.0.0/13',
-            '104.24.0.0/14',
-            '108.162.192.0/18',
-            '131.0.72.0/22',
-            '141.101.64.0/18',
-            '162.158.0.0/15',
-            '172.64.0.0/13',
-            '173.245.48.0/20',
-            '188.114.96.0/20',
-            '190.93.240.0/20',
-            '197.234.240.0/22',
-            '198.41.128.0/17',
-        ];
-
-        $middleware->trustProxies(
-            at: $cfCidrs,
-            headers: Request::HEADER_X_FORWARDED_FOR
-                | Request::HEADER_X_FORWARDED_HOST
-                | Request::HEADER_X_FORWARDED_PORT
-                | Request::HEADER_X_FORWARDED_PROTO
-        );
+    })->withMiddleware(function(Middleware $middleware){
+        $middleware->trustProxies("*");
     })
+    
 
     ->withExceptions(function (Exceptions $exceptions) {
         //

170d33ccb49e64c152e4e5e1006d19d909924265
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-30 19:44:51 +0700

    fix url forscheme https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 // use Illuminate\Pagination\Paginator;
 
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') != 'local') {
-            \URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
